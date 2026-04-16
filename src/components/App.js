1396fddbf4989208c627afe3b6e9381c39d12a6f
sudev <sudev1997@gmail.com>
2026-04-16 19:37:45 +0530

    golf-slide

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,38 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
+            posi: 0,
+            ballPosition: { left: "0px", position: "relative" }
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
 
     buttonClickHandler() {
-   
-   }
+        this.setState({ renderBall: true })
+    }
     renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+        if (this.state.renderBall) {
+            return <div className="ball" style={this.state.ballPosition}></div>
+        } else {
+            return <button className="start" onClick={this.buttonClickHandler} >Start</button>
+        }
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        window.addEventListener('keydown', (e) => {
+            if (e.keyCode === 39 && this.state.renderBall) {
+                this.setState((prevState) => {
+                    const newPos = prevState.posi + 5;
+                    return {
+                        posi: newPos,
+                        ballPosition: { ...prevState.ballPosition, left: `${newPos}px`, position: "relative" }
+                    };
+                })
+            }
+
+        });
     }
 
     render() {
